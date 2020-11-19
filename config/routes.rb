92d6ce5703ffa74d8b31bbf0656a1d71faece358
”Kanta” <k.kanta0826@gmail.com>
2020-11-19 22:13:39 +0900

    11/20

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root to: "home#index"
   resources :products
   resources :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  
 end
